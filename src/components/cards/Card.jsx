3e86aef12e91eb5b20620abdcaf602a07412968a
rahulio96 <rahulgithub96@gmail.com>
2024-02-27 16:26:19 -0800

    Basic layout made

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,14 @@
+import './cards.css'
+
+function Card() {
+
+  return (
+    <div className="card">
+        <p>Image</p>
+        <p>Description</p>
+        <button>Click me!</button>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/cards/CardContainer.jsx
@@ -0,0 +1,26 @@
+import Card from './Card'
+
+function CardContainer() {
+
+  return (
+    <div className='card-container'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default CardContainer
